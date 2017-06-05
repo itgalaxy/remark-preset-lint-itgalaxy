f5a306d4c99b4f1218b09a7893104e0f2c609524
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2017-06-05 23:53:14 +0300

    Style: integrate `prettier`.

--- __tests__/index.js
+++ __tests__/index.js
@@ -11,54 +11,56 @@ function isObject(obj) {
     return typeof obj === 'object' && obj !== null;
 }
 
-test('should have property `plugin`', (t) => {
+test('should have property `plugin`', t => {
     t.true(isObject(config.plugins), 'plugin is object');
 });
 
-test('should have rules in property `lint` in `plugin` property', (t) => {
+test('should have rules in property `lint` in `plugin` property', t => {
     t.true(Object.keys(config.plugins).length > 0, 'exist rules');
 });
 
-test('should have property `settings`', (t) => {
+test('should have property `settings`', t => {
     t.true(isObject(config.settings), 'settings is object');
 });
 
-test('should have no error on valid syntax', (t) => {
+test('should have no error on valid syntax', t => {
     t.plan(2);
 
     return globby(['fixtures/**/*.md'], {
         absolute: true,
         cwd: path.dirname(__filename)
-    })
-        .then(
-            (filePaths) => Promise.all(filePaths.map(
-                (filePath) => new Promise(
-                    (resolve, reject) => fs.readFile(filePath, (error, contents) => {
+    }).then(filePaths =>
+        Promise.all(
+            filePaths.map(filePath =>
+                new Promise((resolve, reject) =>
+                    fs.readFile(filePath, (error, contents) => {
                         if (error) {
                             return reject(error);
                         }
 
                         return resolve(contents);
                     })
-                )
-                    .then(
-                        (contents) => new Promise(
-                            (resolve, reject) => remark()
-                                .use(config.plugins)
-                                .process(contents.toString(), (error, file) => {
-                                    if (error) {
-                                        return reject(error);
-                                    }
+                ).then(contents =>
+                    new Promise((resolve, reject) =>
+                        remark()
+                            .use(config.plugins)
+                            .process(contents.toString(), (error, file) => {
+                                if (error) {
+                                    return reject(error);
+                                }
 
-                                    return resolve(file);
-                                })
-                        )
-                            .then((file) => {
-                                t.true(file.messages.length === 0, `no lint error in ${filePath}`);
-
-                                return true;
+                                return resolve(file);
                             })
-                    )
-            ))
-        );
+                    ).then(file => {
+                        t.true(
+                            file.messages.length === 0,
+                            `no lint error in ${filePath}`
+                        );
+
+                        return true;
+                    })
+                )
+            )
+        )
+    );
 });
